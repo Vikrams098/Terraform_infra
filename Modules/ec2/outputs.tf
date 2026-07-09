5f6c4dc615ac55b0cda5b456bdd2071ed313c358
Vikrams098 <sridharvikram026@gmail.com>
2026-07-09 21:44:55 +0530

    fix: correct resource names in module outputs

--- Modules/ec2/outputs.tf
+++ Modules/ec2/outputs.tf
@@ -1,19 +1,19 @@
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.this.id
+  value       = aws_instance.Sample_EC2.id
 }
 
 output "private_ip" {
   description = "Private IP address of the instance"
-  value       = aws_instance.this.private_ip
+  value       = aws_instance.Sample_EC2.private_ip
 }
 
 output "public_ip" {
   description = "Public IP address (non-empty only for instances in a public subnet)"
-  value       = aws_instance.this.public_ip
+  value       = aws_instance.Sample_EC2.public_ip
 }
 
 output "security_group_id" {
   description = "ID of the instance security group"
-  value       = aws_security_group.this.id
+  value       = aws_security_group.infra_SG.id
 }
--- Modules/vpc/outputs.tf
+++ Modules/vpc/outputs.tf
@@ -1,44 +1,44 @@
 output "vpc_id" {
   description = "ID of the VPC"
-  value       = aws_vpc.this.id
+  value       = aws_vpc.Sample_vpc.id
 }
 
 output "vpc_cidr" {
   description = "CIDR block of the VPC"
-  value       = aws_vpc.this.cidr_block
+  value       = aws_vpc.Sample_vpc.cidr_block
 }
 
 output "internet_gateway_id" {
   description = "ID of the Internet Gateway"
-  value       = aws_internet_gateway.this.id
+  value       = aws_internet_gateway.sample_igw.id
 }
 
 output "public_subnet_ids" {
   description = "IDs of the public subnets"
-  value       = aws_subnet.public[*].id
+  value       = aws_subnet.sample_public[*].id
 }
 
 output "private_subnet_ids" {
   description = "IDs of the private subnets"
-  value       = aws_subnet.private[*].id
+  value       = aws_subnet.sample_private[*].id
 }
 
 output "nat_gateway_id" {
   description = "ID of the NAT Gateway (empty if disabled)"
-  value       = var.enable_nat_gateway ? aws_nat_gateway.this[0].id : null
+  value       = var.enable_nat_gateway ? aws_nat_gateway.sample_nat[0].id : null
 }
 
 output "nat_gateway_public_ip" {
   description = "Public IP of the NAT Gateway EIP (empty if disabled)"
-  value       = var.enable_nat_gateway ? aws_eip.nat[0].public_ip : null
+  value       = var.enable_nat_gateway ? aws_eip.sample_nat[0].public_ip : null
 }
 
 output "public_route_table_id" {
   description = "ID of the public route table"
-  value       = aws_route_table.public.id
+  value       = aws_route_table.sample_public_rt.id
 }
 
 output "private_route_table_id" {
   description = "ID of the private route table"
-  value       = aws_route_table.private.id
+  value       = aws_route_table.sample_private_rt.id
 }
